9257f13a45b2eef698e64108059069b161d61c75
José Luiz Dantas de Lima e Silva <dantas.luiz1@academico.ifrn.edu.br>
2022-09-23 20:50:57 -0300

    Add routes for comment

--- api/urls/__init__.py
+++ api/urls/__init__.py
@@ -1,6 +1,8 @@
 from .post import post_router
+from .comment import comment_router
 from django.urls import path, include
 
 urlpatterns = [
     path("", include(post_router.urls)),
+    path("", include(comment_router.urls)),
 ]
