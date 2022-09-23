8583024424c05be40e730683c2d80b2aa33c14c5
José Luiz Dantas de Lima e Silva <dantas.luiz1@academico.ifrn.edu.br>
2022-09-22 21:32:36 -0300

    Add routes for obtain tokens

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 from users.views import CustomUserViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 router_users = routers.SimpleRouter()
 router_users.register("users", CustomUserViewSet, basename="users")
 
 urlpatterns = [
     path("", include(router_users.urls)),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
 ]
