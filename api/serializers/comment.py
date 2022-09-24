a628fe5068f9d7b848f21e4bce67c844b34996d5
José Luiz Dantas de Lima e Silva <dantas.luiz1@academico.ifrn.edu.br>
2022-09-24 11:44:33 -0300

    Add new serializer to represent comment in like

--- api/serializers/comment.py
+++ api/serializers/comment.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 from api.models.comment import Comment
 from api.serializers.post import PostCommentListSerializer, PostRetrieveSerializer
 from users.serializers import CustomUserShortSerializer, CustomUserRetrieveSerializer
@@ -8,18 +8,25 @@ class CommentCreateOrUpdateSerializer(ModelSerializer):
     class Meta:
         model = Comment
         fields = ["id", "author", "post", "content"]
-        extra_kwargs = {"author": {"read_only": True}, "post": {"read_only": True}}
+        extra_kwargs = {"author": {"read_only": True}}
 
 
 class CommentListSerializer(ModelSerializer):
 
     author = CustomUserShortSerializer()
-    post = PostCommentListSerializer()
+    post = PostCommentListSerializer()    
 
     class Meta:
         model = Comment
         fields = ["id", "author", "post", "content"]
 
+    
+    def to_representation(self, instance):
+        
+        response = super().to_representation(instance)
+        response["likes"] = instance.likes.count()
+
+        return response
 
 class CommentRetrieveSerializer(ModelSerializer):
 
@@ -29,3 +36,19 @@ class CommentRetrieveSerializer(ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
+    
+    def to_representation(self, instance):
+        
+        response = super().to_representation(instance)
+        response["likes"] = instance.likes.count()
+
+        return response
+
+class CommentLikeSerializer(ModelSerializer):
+
+    author = CharField(source="author.name")
+    post = PostCommentListSerializer()
+
+    class Meta:
+        model = Comment
+        fields = ["id", "author", "post", "content"]
